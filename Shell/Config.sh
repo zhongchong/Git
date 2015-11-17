6856641458175fb29baee9d99f6621e9f42859cb
zhongchong <41172680@qq.com>
2015-11-17 14:15:17 +0800

    更换license文件

--- Shell/Config.sh
+++ Shell/Config.sh
@@ -1,12 +1,9 @@
+#! /bin/sh
 
 # 配置全局信息
 git config --global user.name "zhongchong"
 git config --global user.email 41172680@qq.com
-git config --global core.editor 
-
-#bash ls显示中文文件名
-#etc\git-completion.bash
-#alias ls='ls --show-control-chars --color=auto'
+git config --global core.editor vim
 
 # 生成密钥
 ssh-keygen -t rsa -C "z_chong@126.com"
